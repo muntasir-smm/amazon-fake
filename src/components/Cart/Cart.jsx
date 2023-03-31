6845dc090d71f516da915f4011fcb5b74e04ac9b
muntasir-smm <munna112ru@gmail.com>
2023-03-31 20:40:09 +0600

    emma-jhon finish 1.00

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,13 +4,17 @@ import "./Cart.css";
 const Cart = ({ cart }) => {
   //   const { cart } = props;
 
-  console.log(cart);
-
   let totalPrice = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    totalPrice = totalPrice + product.price;
+    // if (product.quantity === 0) {
+    //   product.quantity = 1;
+    // }
+    // product.quantity == product.quantity || 1;
+    totalPrice = (totalPrice + product.price) * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
 
   const tax = (totalPrice * 7) / 100;
@@ -19,7 +23,7 @@ const Cart = ({ cart }) => {
   return (
     <div className="cart">
       <h3>Order Summary</h3>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${totalPrice}</p>
       <p>Total Shipping: ${totalShipping}</p>
       <p>
